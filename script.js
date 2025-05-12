c05738203917bc69111838fb9482d22392646b1d
murjanii <153760405+murjanii@users.noreply.github.com>
2025-05-12 23:36:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    // Trim leading spaces
+    str = str.trimStart();
+
+    // Find the index of the first space
+    let spaceIndex = str.indexOf(' ');
+
+    // If no space found, return the entire string
+    if (spaceIndex === -1) {
+        return str;
+    }
+
+    // Return the substring from start to the first space
+    return str.substring(0, spaceIndex);
 }
+console.log(firstWord('see and stop'));    // Output: 'see'
+console.log(firstWord(' Hello World!'));   // Output: 'Hello'
+console.log(firstWord('12345'));           // Output: '12345'
+console.log(firstWord(''));                // Output: ''
 
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
